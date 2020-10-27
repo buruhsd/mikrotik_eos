b2cd4f779731b288cd0ec4fe1ad25fbea62fab20
buruhsd <buruhsd@outlook.co.id>
2020-10-27 15:19:49 +0700

    device nas ip

--- resources/views/livewire/device/create.blade.php
+++ resources/views/livewire/device/create.blade.php
@@ -30,7 +30,7 @@
         </div>
         <div class="">
               <div class="mb-4">
-                  <label for="exampleFormControlInput1" class="block text-gray-700 text-sm font-bold mb-2">device_port_api:</label>
+                  <label for="exampleFormControlInput1" class="block text-gray-700 text-sm font-bold mb-2">device_nas_api:</label>
                   <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1" placeholder="Enter Title" wire:model="device_nas_ip">
                   @error('device_nas_ip') <span class="text-red-500">{{ $message }}</span>@enderror
               </div>
